e1ca632fead951fcf6f508ce9913571b9778911b
Steven Ke <spp72kimo@gmail.com>
2021-12-26 20:57:54 +0800

    Add count

--- final_number_game.py
+++ final_number_game.py
@@ -5,12 +5,14 @@ import random
 MIN = 0
 MAX = 100
 ans = random.randint(1, 99) # 隨機產生一個終極數字
-print('終極密碼：', ans)
+#print('終極密碼：', ans) # 顯示密碼答案
 
+count = 0 # 設定猜題次數
 key = input('0 ~ 100 請猜密碼：')
 key = int(key)
 
 while True: # 判斷數字區間
+	count += 1
 	if key == ans:
 		print('恭喜答對！')
 		break
@@ -18,6 +20,7 @@ while True: # 判斷數字區間
 		MAX = key
 	elif key < ans:
 		MIN = key
+	print('已經猜了', count, '次\n')
 	print(MIN, '~', MAX, end = '')
 	key = input(' 請猜密碼：')
 	key = int(key)
